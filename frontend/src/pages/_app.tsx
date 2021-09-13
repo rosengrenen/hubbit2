cbbb96ec96777bc6f8ea27b92bbf9e348586e97e
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2021-09-14 01:09:59 +0200

    redirect to login if not logged in error is returned

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,15 +4,26 @@ import '../global-styles/groups.scss';
 
 import React from 'react';
 
-import { createClient, Provider } from 'urql';
+import { createClient, defaultExchanges, errorExchange, Provider } from 'urql';
 
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 
 function HubbitApp({ Component, pageProps }) {
+  const error = errorExchange({
+    onError: error => {
+      for (const e of error.graphQLErrors) {
+        if (e.extensions['code'] === 'NOT_LOGGED_IN') {
+          window.location.href = `${window.location.origin}/api/auth/gamma/login?from=${window.location.href}`;
+        }
+      }
+    },
+  });
+
   const client = createClient({
     // TODO(vidarm): Set this as an environment variable
     url: typeof window === 'undefined' ? 'http://hubbit-backend:8080' : '/api/graphql',
+    exchanges: [error, ...defaultExchanges],
   });
 
   return (
