89c63e9655d6e80e6bdbd8900425d3dcd1fb7ba8
Vidar Magnusson <git@vidarmagnusson.com>
2021-09-14 17:55:05 +0200

    Fix error/loading pages & began work on navbar

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,14 +2,51 @@ import React from 'react';
 
 import styles from './Header.module.scss';
 
+enum Tab {
+  ME,
+  IN_HUB,
+  ALL_STATS,
+  MY_STATS,
+}
+
 const Header = () => {
+  const activeTab: Tab = getCurrentTab();
+
   return (
-    <header>
-      <h1 className={styles.hContainer}>
-        <a className={styles.title}>Who is in the Hubb?</a>
+    <header className={styles.hContainer}>
+      <h1>
+        <a className={styles.title} href="/">
+          Who is in the Hubb?
+        </a>
       </h1>
+      <nav>
+        <ul className={styles.menu}>
+          <li className={activeTab === Tab.ME ? styles.active : ''}>
+            <a href="/">ME</a>
+          </li>
+          <li className={activeTab === Tab.IN_HUB ? styles.active : ''}>
+            <a href="/">SMURFS IN THE HUBB</a>
+          </li>
+          <li className={activeTab === Tab.ALL_STATS ? styles.active : ''}>
+            <a href="/">STATS</a>
+          </li>
+          <li className={activeTab === Tab.MY_STATS ? styles.active : ''}>
+            <a href="/">MY STATS</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
+
+function getCurrentTab(): Tab {
+  const path = window.location.pathname;
+  switch (path) {
+    case '/':
+      return Tab.IN_HUB;
+    default:
+      return Tab.IN_HUB;
+  }
+}
 
 export default Header;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,18 +3,21 @@ import React from 'react';
 import { useCurrentSessionsQuery } from '../__generated__/graphql';
 import ActiveGroupsList from '../components/active-group-list/ActiveGroupsList';
 import ActiveUsersList from '../components/active-users-list/ActiveUsersList';
+import Error from '../components/error/Error';
+import LoadingData from '../components/loading-data/LoadingData';
 
 import styles from './index.module.scss';
 
 const Home = () => {
   const [{ data, fetching, error }] = useCurrentSessionsQuery();
 
-  if (fetching) {
-    return <div>Loading...</div>;
+  if (session.fetching) {
+    return <LoadingData />;
   }
 
-  if (error) {
-    return <div>Error...</div>;
+  if (session.error) {
+    console.error('Error:', session.error);
+    return <Error />;
   }
 
   return (
